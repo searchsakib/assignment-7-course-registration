178a054ef9dd6b4e5d9aa845b29dd1e3e10eb35f
searchsakib <searchsakib@gmail.com>
2023-09-15 11:25:53 +0600

    dynamically added card data

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="max-w-[1320px] bg-[#F3F3F3] mx-auto">
-      <h1 className="text-center font-bold text-3xl m-12">
+    <div className="max-w-[1320px]  mx-auto">
+      <h1 className="text-center font-bold text-3xl p-12">
         Course Registration
       </h1>
       <Home></Home>
