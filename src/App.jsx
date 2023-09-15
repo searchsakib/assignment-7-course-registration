566f2026f1a125c082f8cdd891b3aa70fcdd7dd5
searchsakib <searchsakib@gmail.com>
2023-09-15 07:53:37 +0600

    card added in Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <>
-      <h1>Course Registration</h1>
+    <div className="max-w-[1320px] bg-[#F3F3F3]">
+      <h1 className="text-center font-bold text-3xl m-12">
+        Course Registration
+      </h1>
       <Home></Home>
-    </>
+    </div>
   );
 }
 
